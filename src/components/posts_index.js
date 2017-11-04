cf9f40aacc10ec3fb9eef0d0021af54298e53000
german <gpereira@travelit.com.ar>
2017-11-04 17:03:22 -0300

    muestro lista de posteos

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,16 +1,37 @@
+import _ from "lodash";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
 
 class PostsIndex extends Component {
   componentDidMount() {
-    console.log(this.props.fetchPosts());
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li key={post.id} className="list-group-item">
+          {post.title}
+        </li>
+      );
+    });
   }
 
   render() {
-    return <div>Index</div>;
+    return (
+      <div>
+        <h3>Posts</h3>
+        <ul className="list-group">{this.renderPosts()}</ul>
+      </div>
+    );
   }
 }
-export default connect(null, {
+
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, {
   fetchPosts: fetchPosts
 })(PostsIndex);
