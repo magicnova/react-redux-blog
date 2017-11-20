cf08ed8ab3b2dc214e51f8a3e136bb6a57bf75e6
german <gpereira@travelit.com.ar>
2017-11-20 14:41:50 -0300

    se agrega esrilos a los mensajes de error

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,11 +2,16 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 class PostsNew extends Component {
   renderField(field) {
+    const { meta } = field;
+    const className = `form-group ${meta.touched && meta.error
+      ? "has-danger"
+      : ""}`;
+
     return (
-      <div className=" form-group">
-        <label>{field.label}</label>
-        <input className="form-control" type="text" {...field.input} />
-        {field.meta.touched ? field.meta.error : ""}
+      <div className={className}>
+        <label> {field.label} </label>{" "}
+        <input className="form-control" type="text" {...field.input} />{" "}
+        <div className="text-help"> {meta.touched ? meta.error : ""} </div>{" "}
       </div>
     );
   }
@@ -23,21 +28,20 @@ class PostsNew extends Component {
           name="title"
           label="Title for post"
           component={this.renderField}
-        />
+        />{" "}
         <Field
           name="categories"
           label="Categories"
           component={this.renderField}
-        />
+        />{" "}
         <Field
           name="content"
           label="Post Content"
           component={this.renderField}
         />
-
         <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
+          Submit{" "}
+        </button>{" "}
       </form>
     );
   }
