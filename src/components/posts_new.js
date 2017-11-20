cb02dfe2db9f6d186dc56fea86e3b924228a5740
german <gpereira@travelit.com.ar>
2017-11-20 15:09:55 -0300

    redirect al insertar post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -20,7 +20,9 @@ class PostsNew extends Component {
   }
 
   onSubmit(values) {
-    this.props.createPost(values);
+    this.props.createPost(values, () => {
+      this.props.history.push("/");
+    });
   }
 
   render() {
