b09d303a05d685dcc21e4595d3c0e176ca4f1bab
german <gpereira@travelit.com.ar>
2017-11-29 23:23:30 -0300

    se borra post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions';
+import { Link } from 'react-router-dom';
+import { fetchPost, deletePost } from '../actions';
 
 class PostsShow extends Component {
 	componentDidMount() {
-		const id = this.props.match.params.id;
-		this.props.fetchPost(id);
+		if (!this.props.post) {
+			const id = this.props.match.params.id;
+			this.props.fetchPost(id);
+		}
 	}
+
+	onDeleteClick() {
+		const id = this.props.match.params.id;
+		this.props.deletePost(id, () => {
+			this.props.history.push('/');
+		});
+	}
+
 	render() {
 		const { post } = this.props;
 
@@ -16,6 +27,13 @@ class PostsShow extends Component {
 
 		return (
 			<div>
+				<Link to="/">Back to index</Link>
+				<button
+					onClick={this.onDeleteClick.bind(this)}
+					className="btn btn-danger pull-xs-right"
+				>
+					Delete
+				</button>
 				<h3>{post.title}</h3>
 				<h6>Categories: {post.categories}</h6>
 				<p>Content: {post.content}</p>
@@ -25,8 +43,7 @@ class PostsShow extends Component {
 }
 
 function mapStateToProps({ posts }, ownProps) {
-	console.log(posts[ownProps.match.params.id]);
 	return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
